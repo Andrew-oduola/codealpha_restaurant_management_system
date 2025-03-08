80dc646865eeb54e3857b9a579e89877eed3f850
Drew <ayobamioduola13@gmail.com>
2025-03-08 12:52:02 +0100

    add image to field

--- menu/models.py
+++ menu/models.py
@@ -7,6 +7,8 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=50)
     available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='menu_images/', null=True, blank=True)
+
 
     def __str__(self):
         return self.name
