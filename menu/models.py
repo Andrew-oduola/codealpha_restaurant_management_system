2a90265d694a51729ef5c08eca2f3275b8295bfd
Drew <ayobamioduola13@gmail.com>
2025-03-09 13:27:44 +0100

    added category model to menu app

--- menu/models.py
+++ menu/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='menu_category_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=50)
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
     available = models.BooleanField(default=True)
     image = models.ImageField(upload_to='menu_images/', null=True, blank=True)
 
 
     def __str__(self):
         return self.name
+    
+# TODO 1: Create a model called Category
+# TODO 2: Create a model called
