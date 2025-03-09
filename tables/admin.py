1bf8440b5b5657ae81fee8496afa180d92cbaa46
Drew <ayobamioduola13@gmail.com>
2025-03-09 15:06:14 +0100

    feat(table): avaliable tables endpoint

--- tables/admin.py
+++ tables/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Table
 
 # Register your models here.
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ['table_number', 'capacity', 'is_occupied', 'location']
+    list_filter = ['is_occupied']
+    search_fields = ['table_number', 'location']
+    list_editable = ['is_occupied']
+    list_per_page = 10
+    list_display_links = ['table_number']
+    list_max_show_all = 100
+    # list_select_related = ['location']
+    list_display_links = ['table_number']
--- tables/views.py
+++ tables/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from .serializers import TableSerializer
 from .models import Table
@@ -8,4 +9,8 @@ class TableViewSet(viewsets.ModelViewSet):
     queryset = Table.objects.all()
     serializer_class = TableSerializer
 
-
+    @action(detail=False, methods=['get'], url_path='available', url_name='available')
+    def available_table(self, request):
+        available_tables = Table.objects.filter(is_occupied=False)
+        serializer = self.get_serializer(available_tables, many=True)
+        return Response(serializer.data)
