8b0be725b88af239f17081c65cfe1d703829fd89
Drew <ayobamioduola13@gmail.com>
2025-03-14 05:28:59 +0100

    feat(menu):   Add IsAdminOrReadOnly permission to menu endpoints

--- menu/views.py
+++ menu/views.py
@@ -4,21 +4,23 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import MenuItemSerializer, MenuCategorySerializer
 from .models import MenuItem, MenuCategory
-
+from .permissions import IsAdminOrReadOnly
 
 class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
 
+    
     @action(detail=False, methods=['get'], url_path='available', url_name='available')
     def available_menu(self, request):
         available_items = MenuItem.objects.filter(available=True)
         serializer = self.get_serializer(available_items, many=True)
         return Response(serializer.data)
 
-
 class MenuCategoryViewSet(viewsets.ModelViewSet):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+    
