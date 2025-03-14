9162c6c3dd980b166115e750f2ca70a30a49e1fc
Drew <ayobamioduola13@gmail.com>
2025-03-14 12:07:21 +0100

    fix(test_menu): remove unneccessary comments

--- menu/tests/test_menu.py
+++ menu/tests/test_menu.py
@@ -121,43 +121,4 @@ class TestCreateUpdateDeleteMenu(APITestCase):
         response = self.api_client.delete(f'/api/menu/items/{self.menu.id}/')
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    
-
-# @pytest.mark.django_db
-# class TestCreateUpdateDeleteInventory(APITestCase):
-#     def setUp(self):
-#         self.api_client = APIClient()
-#         self.user = User.objects.create_user(email='testuser@gmail.com', password='testpass')
-#         self.inventory = Inventory.objects.create(item_name="Test Item", quantity=5, reorder_level=10)
-        
-#     def test_if_authenticated_user_can_create_inventory(self):
-#         self.api_client.force_authenticate(user=self.user)
-#         response = self.api_client.post('/api/inventory/', {
-#             'item_name': 'Test Item 2', 
-#             'quantity': 10, 
-#             'reorder_level': 5,
-#             'unit': 50.00
-#             })
-#         assert response.status_code == status.HTTP_201_CREATED
-
-#     def test_if_authenticated_user_can_update_inventory(self):
-#         self.api_client.force_authenticate(user=self.user)
-#         response = self.api_client.put(f'/api/inventory/{self.inventory.id}/', {
-#             'item_name': 'Test Item 2', 
-#             'quantity': 10, 
-#             'reorder_level': 5,
-#             'unit': 50.00
-#             })
-#         assert response.status_code == status.HTTP_200_OK
-
-#     def test_if_authenticated_user_can_delete_inventory(self):
-#         self.api_client.force_authenticate(user=self.user)
-#         response = self.api_client.delete(f'/api/inventory/{self.inventory.id}/')
-#         assert response.status_code == status.HTTP_204_NO_CONTENT
-
-# @pytest.mark.django_db
-# class TestInventoryModel:
-#     def test_inventory_model(self):
-#         inventory = baker.make('inventory.Inventory')
-#         assert isinstance(inventory, Inventory)
-#         assert inventory.__str__() == inventory.item_name
+ 
